f34968eb2fa47f9f69a643a392bed946d333ed5d
U10416020 <wkps19961015@gmail.com>
2016-03-19 02:27:14 +0800

    Create a class named TestBigDouble
    Create the main class to test BigDouble and display the result.

---
+++ TestBigDouble.java
@@ -0,0 +1,21 @@
+//U10416020
+
+import java.util.*;
+
+public class TestBigDouble{
+	//Main method
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		//Prompt user to enter doubles
+		System.out.println("Enter two double: ");
+		String num1 = input.next();
+		String num2 = input.next();
+		
+		//Create a object of BigDouble
+		BigDouble test = new BigDouble(num1, num2);
+		
+		//Print the result
+		System.out.println(test.getPrint());		
+	}
+}
